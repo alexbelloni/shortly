d0e0282183c0d9ddd2ee5453faa5ef41bab355b7
Alexandre Belloni Alves <alexbelloni@outlook.com>
2020-03-17 16:32:45 -0400

    calling bitly API

--- js/app.js
+++ js/app.js
@@ -1,37 +1,76 @@
-window.addEventListener("load",()=>{
-    document.querySelector("#hamburger").addEventListener("click",e=>{
+window.addEventListener("load", () => {
+    document.querySelector("#hamburger").addEventListener("click", e => {
         const mobileClasses = document.querySelector("#menu-mobile").getAttribute("class");
-        document.querySelector("#menu-mobile").setAttribute("class", mobileClasses === "part close"? "part open" : "part close");
+        document.querySelector("#menu-mobile").setAttribute("class", mobileClasses === "part close" ? "part open" : "part close");
         const imageClasses = document.querySelector("#working").getAttribute("class");
-        document.querySelector("#working").setAttribute("class", imageClasses === "part close"? "part" : "part close");
+        document.querySelector("#working").setAttribute("class", imageClasses === "part close" ? "part" : "part close");
     });
 
-    document.querySelector("#btn-shorten-it").addEventListener("click",e=>{
-        function createElement(atype, aclass, atext, anid){
+    document.querySelector("#btn-shorten-it").addEventListener("click", e => {
+        function createElement(atype, aclass, atext, anid) {
             const element = document.createElement(atype);
             element.className = aclass || "";
             element.innerText = atext || "";
             element.id = anid || "";
             return element;
         }
-        const userLink = document.querySelector("#user-link").value;
-        const id = Math.random().toString().substr(0,6);
-        const shortlyLink = `https://short.ly/${id}`;
+        function getTrunkedLink(link) {
+            return `${link.substr(0, 30)}...`;
+        }
+        function createElements(id, shortlyLink) {
+            const nodeLink = createElement("div", "link");
+            nodeLink.appendChild(createElement("span", null, getTrunkedLink(userLink)));
+            nodeLink.appendChild(createElement("span", null, shortlyLink, id));
+            const nodeButton = createElement("button", null, "Copy");
+            nodeButton.addEventListener("click", () => {
+                ClipboardAPIClipboardWrite(shortlyLink);
+                nodeButton.innerText = "Copied!"
+                nodeButton.className = "copied";
+            });
+            nodeLink.appendChild(nodeButton);
+            document.querySelector("#links").appendChild(nodeLink);
+        }
 
-        const nodeLink = createElement("div", "link");
-        nodeLink.appendChild(createElement("span", null, userLink));
-        nodeLink.appendChild(createElement("span", null, shortlyLink));
-        const nodeButton = createElement("button", null, "Copy", id);
-        nodeButton.addEventListener("click", ()=>{ 
-            nodeButton.innerText = "Copied!"
-            nodeButton.className = "copied";
-            alert(shortlyLink);
-         });
-        nodeLink.appendChild(nodeButton);
-        document.querySelector("#links").appendChild(nodeLink);        
+        const userLink = document.querySelector("#user-link").value;
+        shortify(userLink, success => {
+            const id = `sh${Math.random().toString().substr(2, 4)}`;
+            createElements(id, success);
+        }, error => alert(error));
     });
 })
 
-function cu(){
-    alert('hiii')
+function ClipboardAPIClipboardWrite(value) {
+    if (document.activeElement) {
+        document.activeElement.blur();
+    }
+    window.focus();
+    navigator.permissions.query({ name: "clipboard-write" }).then(result => {
+        if (result.state == "granted" || result.state == "prompt") {
+            navigator.clipboard.writeText(value);
+        }
+    });
+}
+
+function shortify(userLink, success, error) {
+    if (!userLink) return
+
+    const data = {
+        "long_url": userLink
+    }
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://api-ssl.bitly.com/v4/shorten");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Authorization", `Bearer 0c0f6a3e164e8f1d3da13b09ab9587b9e937ec14`);
+    xhr.addEventListener("readystatechange", function () {
+        const responseJson = JSON.parse(this.responseText);
+        if (responseJson.errors && responseJson.errors.length > 0) {
+            error(responseJson.errors.map(e => `${e.field}: ${e.error_code}`).join(", "));
+        } else {
+            if (this.readyState === 4) {
+                success(responseJson.link);
+
+            }
+        }
+    });
+    xhr.send(JSON.stringify(data));
 }
