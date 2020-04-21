e295b831fe74460a23f7824415728a5338b11f6e
Alexandre Belloni Alves <alexbelloni@outlook.com>
2020-04-21 18:43:24 -0400

    updated netlify functions

--- functions/config.js
+++ functions/config.js
@@ -1,5 +1,5 @@
 // *** Netlify Functions for getting environment variables 
-const { FIREBASE_APIKEY, FIREBASE_APPID, FIREBASE_PROJECTID } = process.env;
+const { FIREBASE_APIKEY, FIREBASE_APPID, FIREBASE_PROJECTID, AUTH_BITLY, AUTH_REBRANDLY } = process.env;
 
 exports.handler = (event, context, callback) => {
     return callback(null, {
